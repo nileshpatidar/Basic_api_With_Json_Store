5dfbd7706239ac53dc5c9aa15123abb27586ea2f
NILESH PATIDAR <35222555+nileshpatidar@users.noreply.github.com>
2021-03-08 12:22:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,41 @@ const express = require('express');
 const app = express(); 
 const { getUserData, saveUserData } = require("./commonFumction/common");
 const Authchecker = require("./commonFumction/middelware");
-  
+ 
+let jwt = require('jsonwebtoken');
+var md5 = require('md5')
+var multer = require('multer')
+var mkdirp = require('mkdirp');
+mkdirp('uploads/profile/');
+let SECRET_KEY = "nileshsecretkey1234567890"
+ 
+
+
+
+
+var localstorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/profile/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+        cb(null, true)
+    } else {
+        return cb('jpeg,jpg ,png file are allowed', false)
+    }
+}
+
+
+var local_upload = multer({
+    storage: localstorage,
+    fileFilter: fileFilter,
+}).single('profile_image')
+
 
 app.get('/', (req, res) => {
     res.statusCode = 200;
