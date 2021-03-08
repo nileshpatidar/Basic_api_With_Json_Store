05361de4be33b3662a2870bba52d41c5bb1ffc58
NILESH PATIDAR <35222555+nileshpatidar@users.noreply.github.com>
2021-03-08 11:18:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<h1>Hello World</h1>');
+// });
 
 const express = require('express')
 let jwt = require('jsonwebtoken');
